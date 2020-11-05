42d95454fe9aed91e3429ef80349faec9402acee
earthday-lim <ssw3832@naver.com>
2020-11-05 21:26:53 +0900

    ...

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: path.join(__dirname, './uploads/') }); //req를 지난 다음에 실행해야 req를 인식할 수 있음
+const { v4: uuidv4 } = require('uuid'); //unique한 key값을 받아오는 모듈
 
 /* modules */
 //const { pool } = require('./modules/mysql-conn');
+const logger = require('./modules/morgan-conn');
 const boardRouter = require('./routes/board');
 const galleryRouter = require('./routes/gallery');
 
@@ -13,13 +17,15 @@ const galleryRouter = require('./routes/gallery');
 app.listen(process.env.PORT, () => {console.log(`http://127.0.0.1:${process.env.PORT}`);}); //콜백없어도 됨
 
 /* Initialize */
-app.set('view-engine', 'pug');
+app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, './views'));//절대경로를 줄 땐 path.join()
 app.locals.pretty = true; //client한테 보내줄 때 정리 잘 해서 보내줄게요
 
 /* middleware */
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+//=> (req, res, next)를 갖고 있는 애들 : req가 모든 정보를 갖고 있고 내가 할 일 하고 next로 내보내주는 애들
+app.use(logger, express.json(), express.urlencoded({extended: false}));
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
 
 /* router */
 app.use('/', express.static(path.join(__dirname, './public')));//절대경로를 줄 땐 path.join()
@@ -29,6 +35,14 @@ app.use('/gallery', galleryRouter);
   const err = new Error();
   next(err); //젤 마지막 app.use()로 보냄
 }); */
+app.get('/test/upload', (req, res, next) => { ///test/upload로 요청이 들어온다면, req,res,next로 가서
+  res.render('test/upload'); //test/upload.pug를 열고 html로 해석해서 보여라
+});
+
+app.post('/test/save', upload.single('upfile'), (req, res, next) => {
+  const { title, upfile } = req.body;
+  res.redirect('/board');
+});
 
 /* error 평범한 에러 */
 app.use((req, res, next) => {
